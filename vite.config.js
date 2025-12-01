32e7261cc3d8f7bda7a2c917249ebced1126b8b4
Omprakasht121 <omprakasht5689@gmail.com>
2025-12-01 08:44:36 +0530

    vercel deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/om-portfolio/',  // 👈 this is the correct place for base
+  base: '/',  // 👈 this is the correct place for base
 })
